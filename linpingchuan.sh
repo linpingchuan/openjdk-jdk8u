6c89663e380936379956a4dbf53b59b472505947
lin pingchuan <paul_lin@foxmail.com>
2020-01-05 00:30:24 +0800

    完成调试机制

--- linpingchuan.sh
+++ linpingchuan.sh
@@ -4,12 +4,12 @@ compile_openjdk8u(){
     export CXXFLAGS="-Wno-error"
     export CPPFLAGS="-Wno-error"
     export CFLAGS="-Wno-error"
-    sudo apt-get install -y libx11-dev libxext-dev libxrender-dev libxrandr-dev libxtst-dev libxt-dev
-    sudo apt-get install ccache -y
+    chuan apt-get install -y libx11-dev libxext-dev libxrender-dev libxrandr-dev libxtst-dev libxt-dev
+    chuan apt-get install ccache -y
     cd /home/lin/openjdk-jdk8u
     # 删除无用的日志
     rm hs_err_pid*.log
-    #sudo make dist-clean
+    #chuan make dist-clean
     current_directory=`pwd | grep -a 'jdk' | grep -a 'u'`
     if [ -n "$current_directory" ];then
         # 指定 bootjdk 为 openjdk8
@@ -21,7 +21,7 @@ compile_openjdk8u(){
         bash configure --with-debug-level=slowdebug --disable-warnings-as-errors --enable-ccache --with-native-debug-symbols=external
     fi
 
-    sudo make CONF=linux-x86_64-normal-server-slowdebug ZIP_DEBUGINFO_FILES=0 ENABLE_FULL_DEBUG_SYMBOLS=1 LOG=debug JOBS=2
+    chuan make CONF=linux-x86_64-normal-server-slowdebug ZIP_DEBUGINFO_FILES=0 ENABLE_FULL_DEBUG_SYMBOLS=1 LOG=debug JOBS=2
 
     export PATH=$PATH:/home/lin/openjdk-jdk8u/build/linux-x86_64-normal-server-slowdebug/jdk/bin/
 }
